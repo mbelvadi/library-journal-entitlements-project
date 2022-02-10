2ddae065589fd3f5d2cd997070caed8917529a0d
Cal Trainor <cal16trainor@gmail.com>
2022-02-09 22:44:01 -0800

    client now hits test PHP endpoint

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,11 +1,27 @@
+import { Button } from 'antd';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Home() {
+  const [randomNumber, setRandomNumber] = React.useState(null);
+
+  const getRandomNumber = async () => {
+    const res = await fetch(
+      'http://localhost/upei-library-journal-project/server/routes/random-number'
+    );
+    const data = await res.json();
+    setRandomNumber(data.randomNumber);
+  };
+
   return (
     <>
       <h1>Home</h1>
       <Link to='/search'>Search</Link>
+      <br />
+      <Button type='primary' onClick={getRandomNumber}>
+        Get Random Number
+      </Button>
+      <div>Random number: {randomNumber}</div>
     </>
   );
 }
