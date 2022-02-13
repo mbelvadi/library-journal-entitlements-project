980a6b5073ce4e7d3a5f1aa63c49c0a002b0dc14
Cal Trainor <cal16trainor@gmail.com>
2022-02-12 23:36:31 -0800

    added route that downloads a file from the server to client

---
+++ server/routes/download-local-file.php
@@ -0,0 +1,20 @@
+<?php
+  $data = json_decode(file_get_contents("php://input"));
+  $file = (property_exists($data, "file")) ? $data->file : "";
+  $filepath = "../PAR-files/{$file}";
+  
+  if (file_exists($filepath)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename='.basename($filepath));
+    header('Content-Transfer-Encoding: binary');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($filepath));
+    ob_clean();
+    flush();
+    readfile($filepath);
+    exit;
+  } 
+?>
