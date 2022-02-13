0f1c7e4d62d84e2f4c5c27facafeb92e6667c008
Cal Trainor <cal16trainor@gmail.com>
2022-02-12 23:36:15 -0800

    added route that returns all .xlsx file names in directory

---
+++ server/routes/list-files.php
@@ -0,0 +1,15 @@
+<?php
+  function getXLSXFiles($path) {
+    if (is_dir($path)) {
+        $res = array();
+        foreach (array_filter(glob($path ."*.xlsx"), 'is_file') as $file) {
+            array_push($res, str_replace($path, "", $file));                
+        }
+        return $res;
+    }
+    return false;
+  }
+
+  $files = getXLSXFiles('../PAR-files/');
+  echo json_encode($files);
+?>
