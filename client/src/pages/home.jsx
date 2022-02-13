3f52e7366e4e31c5d871eb6e6e5135f54291fbb8
Cal Trainor <cal16trainor@gmail.com>
2022-02-12 23:37:00 -0800

    list excel files on server and allow users to download them to local machine

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,9 +1,46 @@
 import { Button } from 'antd';
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export default function Home() {
   const [randomNumber, setRandomNumber] = React.useState(null);
+  const [fileLinks, setFileLinks] = React.useState([]);
+
+  React.useEffect(() => {
+    const getFileLinks = async () => {
+      const data = await (
+        await fetch(
+          'http://localhost/upei-library-journal-project/server/routes/list-files'
+        )
+      ).json();
+      setFileLinks(data);
+    };
+    getFileLinks();
+  }, []);
+
+  function downloadFileToClient(blob, filename) {
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }
+
+  const downloadFile = async (file) => {
+    const res = await fetch(
+      'http://localhost/upei-library-journal-project/server/routes/download-local-file',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          file: file,
+        }),
+      }
+    );
+    res.blob().then((blob) => downloadFileToClient(blob, file));
+  };
 
   const getRandomNumber = async () => {
     const res = await fetch(
@@ -13,11 +50,24 @@ export default function Home() {
     setRandomNumber(data.randomNumber);
   };
 
+  const fileLinkElements = fileLinks.map((link) => {
+    return (
+      <Button
+        type='link'
+        key={link}
+        onClick={() => {
+          downloadFile(link);
+        }}
+      >
+        {link}
+      </Button>
+    );
+  });
+
   return (
     <>
       <h1>Home</h1>
-      <Link to='/search'>Search</Link>
-      <br />
+      <div>{fileLinkElements}</div>
       <Button type='primary' onClick={getRandomNumber}>
         Get Random Number
       </Button>
