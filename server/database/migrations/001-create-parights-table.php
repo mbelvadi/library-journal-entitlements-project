c48563b063525b366994a3ec782bdf24354a6350
Cal Trainor <cal16trainor@gmail.com>
2022-02-17 20:58:46 -0800

    setup pa-rights table migration

---
+++ server/database/migrations/001-create-parights-table.php
@@ -0,0 +1,36 @@
+<?php
+  class OpenDB extends SQLite3 {
+    function __construct() {
+      $this->open('../database/ljp.db');
+    }
+  }
+
+  function createPARightsTable($db) {
+    $sql =<<<EOF
+      CREATE TABLE IF NOT EXISTS PA_RIGHTS
+      (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      title VARCHAR(255) NOT NULL,
+      title_id CHARACTER(20) NOT NULL,
+      print_issn CHARACTER(20),
+      online_issn CHARACTER(20),
+      former_title CHARACTER(20),
+      succeeding_title CHARACTER(20),
+      agreement_code VARCHAR(255) NOT NULL,
+      year INT NOT NULL,
+      collection_name VARCHAR(255),
+      title_metadata_last_modified VARCHAR(255),
+      filename VARCHAR(255) NOT NULL,
+      has_rights CHARACTER(20) NOT NULL);
+    EOF;
+
+    $ret = $db->exec($sql);
+    if(!$ret){
+        echo $db->lastErrorMsg();
+    } 
+    $db->close();
+  }
+
+  function runMigration() {
+    createPARightsTable(new OpenDB());
+  }
+?>
