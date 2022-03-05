260e9c0268e6408b3d11d26e457d595e34a6dfa7
Ben Boyle <me@benboyle.ca>
2022-03-05 16:50:21 -0400

    add filters drop down to search bar

--- client/src/components/search-bar.jsx
+++ client/src/components/search-bar.jsx
@@ -1,21 +1,55 @@
-import { Form, Input, Button, Col } from 'antd';
+import { Form, Input, Button, Col, Dropdown, DatePicker, Card } from 'antd';
+import { FilterFilled, FilterOutlined } from '@ant-design/icons';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-export default function SearchBar(props) {
+export default function SearchBar() {
   const [query, setQuery] = useState('');
+  const [visible, setVisible] = useState(false);
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
+  const handleSubmitSearch = async (e) => {
     e.target.parentElement.children[0].blur();
     navigate(`/search?query=${query}`);
     setQuery('');
   };
 
+  const handleVisibleChange = (flag) => {
+    setVisible(flag);
+  };
+
+  const { RangePicker } = DatePicker;
+
+  const filterFormItem = (name, title, content) => (
+    <Form.Item name={name}>
+      <Card title={title} size='small'>
+        {content}
+      </Card>
+    </Form.Item>
+  );
+
+  const filterForm = (
+    <Form layout='vertical'>
+      {filterFormItem(
+        'yearFilter',
+        'Specific Year',
+        <DatePicker picker='year' bordered={false} />
+      )}
+      {filterFormItem(
+        'yearRangeFilter',
+        'Year Range',
+        <RangePicker picker='year' bordered={false} />
+      )}
+    </Form>
+  );
+
   return (
-    <Form onSubmit={handleSubmit} layout='inline'
-    style={{ marginLeft: 50 }}>
-      <Col span={20}>
+    <Form
+      onSubmit={handleSubmitSearch}
+      layout='inline'
+      style={{ marginLeft: 50 }}
+    >
+      <Col span={18}>
         <Input
           placeholder='Search...'
           size='large'
@@ -24,15 +58,29 @@ export default function SearchBar(props) {
         ></Input>
         <button
           type='submit'
-          onClick={handleSubmit}
+          onClick={handleSubmitSearch}
           style={{ display: 'none' }}
         ></button>
       </Col>
       <Col
-        span={4}
-        style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+        span={6}
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
       >
-        <Button type='primary'>Filters</Button>
+        <Dropdown.Button
+          type='primary'
+          overlay={filterForm}
+          icon={<FilterOutlined />}
+          trigger={['click']}
+          onVisibleChange={handleVisibleChange}
+          visible={visible}
+          onClick={handleSubmitSearch}
+        >
+          Search
+        </Dropdown.Button>
       </Col>
     </Form>
   );
