27943b7232d88cc7fdce3409b2d1c4098b7199ee
Cal Trainor <cal16trainor@gmail.com>
2022-03-08 22:55:57 -0800

    updated delete data function to handle multiple types of queries

--- server/database/util/delete-crkn-data.php
+++ server/database/util/delete-crkn-data.php
@@ -1,13 +1,19 @@
 <?php
-  function deleteOldCrknData($time) {
+  function deleteOldCrknData($mode, $time, $filename) {
     $dbPath = dirname(__DIR__, 1) . '/ljp.db';
     $db = new SQLite3($dbPath);
 
-    $sqlStatement = $db->prepare("DELETE FROM PA_RIGHTS WHERE created_at < :time_cutoff AND is_crkn_record = true");
-
-    $sqlStatement->bindParam(':time_cutoff', $time);
-
-    $sqlStatement->execute();
+    if ($mode === 'time') {
+      $sqlStatement = $db->prepare("DELETE FROM PA_RIGHTS WHERE created_at < :time_cutoff AND is_crkn_record = true");
+      $sqlStatement->bindParam(':time_cutoff', $time);
+      $sqlStatement->execute();
+    } else if ($mode === 'filename') {
+      $sqlStatement = $db->prepare("DELETE FROM PA_RIGHTS WHERE created_at < :time_cutoff AND filename = :filename");
+      $sqlStatement->bindParam(':time_cutoff', $time);
+      $sqlStatement->bindParam(':filename', $filename);
+      $sqlStatement->execute();
+    }
+   
 
     $db->close();
   }
