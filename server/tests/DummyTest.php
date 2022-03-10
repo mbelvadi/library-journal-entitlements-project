7dd4bf0c3378942d775ffe99be4e22ab09d40fb9
Ben Boyle <me@benboyle.ca>
2022-03-10 12:54:32 -0400

    Add GitHub Actions Workflow to Test PHP Backend

---
+++ server/tests/DummyTest.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class DummyTest extends TestCase
+{
+    public function testDummy(): void
+    {
+        $this->assertTrue(true);
+    }
+}
