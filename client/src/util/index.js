c45dfca84dcf57983ef58a19c6f36cab787dcdf8
Cal Trainor <cal16trainor@gmail.com>
2022-03-17 19:21:17 -0700

    moved downloadFileToClient function to util file

--- client/src/util/index.js
+++ client/src/util/index.js
@@ -2,3 +2,15 @@ export const API_URL =
   process.env.NODE_ENV === 'development'
     ? `http://localhost/upei-library-journal-project/server/routes`
     : '/routes';
+
+export const downloadFileToClient = (blob, filename) => {
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.style.display = 'none';
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  window.URL.revokeObjectURL(url);
+};
