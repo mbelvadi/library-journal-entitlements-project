838cb54d360e913f5dd3461555bb42199dc082d7
Cal Trainor <cal16trainor@gmail.com>
2022-03-17 21:01:34 -0700

    added /admin/valid-token endpoint

---
+++ server/routes/admin/valid-token.php
@@ -0,0 +1,16 @@
+<?php
+  require('../../util/error-handling.php');
+  require('../../util/index.php');
+  set_error_handler('apiErrorHandler', E_ALL);
+
+  if(!isset($_GET["adminKey"])) {
+    http_response_code(400);
+    echo json_encode(array("error" => "admin key is required."));
+    return;
+  }
+
+  $isValidAdmin = validAdmin($_GET["adminKey"], '../../database/ljp.db');
+  if(!$isValidAdmin) return;
+
+  echo json_encode(array("message" => "valid token."));
+?>
