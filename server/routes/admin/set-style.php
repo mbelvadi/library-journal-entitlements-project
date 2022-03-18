504e93dd2d00654799a2ea544a6bb6707c284b87
Cal Trainor <cal16trainor@gmail.com>
2022-03-17 21:56:42 -0700

    added /admin/set-style route

---
+++ server/routes/admin/set-style.php
@@ -0,0 +1,28 @@
+<?php
+  require('../../util/error-handling.php');
+  require('../../util/index.php');
+  set_error_handler('apiErrorHandler', E_ALL);
+
+  if(!isset($_POST["adminKey"])) {
+    http_response_code(401);
+    echo json_encode(array("error" => "admin key is required."));
+    return;
+  }
+ 
+  $isValidAdmin = validAdmin($_POST["adminKey"], '../../database/ljp.db');
+  if(!$isValidAdmin) return;
+
+  $config = json_decode(file_get_contents(dirname(__DIR__, 2) . '/config.json'));
+
+
+  $config->style->color = $_POST["color"];
+  $config->style->pageTitle = $_POST["pageTitle"];
+  $config->style->favicon = $_POST["favicon"];
+  $config->style->logo = $_POST["logo"];
+  
+  $jsonData = json_encode($config, JSON_PRETTY_PRINT);
+  file_put_contents(dirname(__DIR__, 2) . '/config.json', $jsonData);
+
+  http_response_code(200);
+  echo json_encode(array("message" => "Successfully updated style config."));
+?>
