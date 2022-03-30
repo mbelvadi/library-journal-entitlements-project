4eaf23f16931c23cb104025e7828831f8e1bea7f
Cal Trainor <cal16trainor@gmail.com>
2022-03-29 19:43:27 -0700

    set admin timeout to 8 hours

--- server/database/migrations/002-create-admin-token-table.php
+++ server/database/migrations/002-create-admin-token-table.php
@@ -4,7 +4,7 @@
       CREATE TABLE IF NOT EXISTS ADMIN_TOKENS
       (token VARCHAR(255) NOT NULL,
       created_at DATETIME NOT NULL DEFAULT (strftime('%s', 'now')),
-      valid_till DATETIME NOT NULL DEFAULT (strftime('%s', 'now', '+10 minutes')));
+      valid_till DATETIME NOT NULL DEFAULT (strftime('%s', 'now', '+8 hours')));
     EOF;
 
     $ret = $db->exec($sql);
