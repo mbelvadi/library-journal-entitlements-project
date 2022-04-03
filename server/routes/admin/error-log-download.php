e3c9fdc3fa6f6e15af3fab0e3b2e2fe23360a223
Cal Trainor <cal16trainor@gmail.com>
2022-04-03 00:06:38 -0700

    allow admins to download error file

---
+++ server/routes/admin/error-log-download.php
@@ -0,0 +1,21 @@
+<?php
+  require('../../util/error-handling.php');
+  set_error_handler('apiErrorHandler', E_ALL);
+
+  $filepath = "../../upload-errors.csv";
+  
+  if (file_exists($filepath)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename='.basename($filepath));
+    header('Content-Transfer-Encoding: binary');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($filepath));
+    ob_clean();
+    flush();
+    readfile($filepath);
+    exit;
+  } 
+?>
