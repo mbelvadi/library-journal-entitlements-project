efdd96befdc7dca64323d143ec1891599a09a533
Ben Boyle <me@benboyle.ca>
2022-04-18 16:38:15 -0300

    simple snapshot testing for TSV generation

--- client/src/pages/search-results.jsx
+++ client/src/pages/search-results.jsx
@@ -19,6 +19,45 @@ const parseParams = (querystring) => {
   return obj;
 };
 
+export const generateExportFileContent = (data, delimeter, unwantedColumns) => {
+  const resultsToExport = data.map((row) => {
+    for (const [oldKey, oldValue] of Object.entries(row)) {
+      const key = oldKey.trim();
+      row[oldKey] = undefined;
+
+      if (key.toLowerCase() === 'is_crkn_record') {
+        row[key] = oldValue ? 'Y' : 'N';
+        continue;
+      }
+
+      if (typeof oldValue === 'string') {
+        // trim out white space
+        row[key] = oldValue.trim();
+      } else if (oldValue === null || oldValue === undefined) {
+        // convert empty entries to empty strings
+        row[key] = '';
+      } else {
+        row[key] = oldValue;
+      }
+    }
+
+    // remove unwanted columns from each row
+    for (const prop of unwantedColumns) {
+      delete row[prop];
+    }
+
+    return row;
+  });
+
+  const header = Object.keys(resultsToExport[0]).join(delimeter);
+
+  const values = resultsToExport
+    .map((row) => Object.values(row).join(delimeter))
+    .join('\n');
+
+  return header + '\n' + values + '\n';
+};
+
 export default function SearchResults() {
   const [searchResults, setSearchResults] = React.useState(null);
   const [displayedData, setDisplayedData] = React.useState([]);
@@ -57,51 +96,12 @@ export default function SearchResults() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [search, apiRoute]);
 
-  const generateExportFileContent = (delimeter, unwantedColumns) => {
-    const resultsToExport = displayedData.map((row) => {
-      for (const [oldKey, oldValue] of Object.entries(row)) {
-        const key = oldKey.trim();
-        row[oldKey] = undefined;
-
-        if (key.toLowerCase() === 'is_crkn_record') {
-          row[key] = oldValue ? 'Y' : 'N';
-          continue;
-        }
-
-        if (typeof oldValue === 'string') {
-          // trim out white space
-          row[key] = oldValue.trim();
-        } else if (oldValue === null || oldValue === undefined) {
-          // convert empty entries to empty strings
-          row[key] = '';
-        } else {
-          row[key] = oldValue;
-        }
-      }
-
-      // remove unwanted columns from each row
-      for (const prop of unwantedColumns) {
-        delete row[prop];
-      }
-
-      return row;
-    });
-
-    const header = Object.keys(resultsToExport[0]).join(delimeter);
-
-    const values = resultsToExport
-      .map((row) => Object.values(row).join(delimeter))
-      .join('\n');
-
-    return header + '\n' + values;
-  }
-
   const onClickDownload = () => {
     const unwantedColumns = ['key', 'created_at', 'id'];
     const delimeter = '\t';
     const fileExtension = 'tsv';
 
-    const fileContent = generateExportFileContent(delimeter, unwantedColumns);
+    const fileContent = generateExportFileContent(displayedData, delimeter, unwantedColumns);
 
     downloadFileToClient(
       new Blob([fileContent], { type: 'text/' + fileExtension }),
